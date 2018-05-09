2a8edc26addbcf2d1ec7b75a6baa842768555925
Niv Dunay <nivdu@mta.ac.il>
2018-05-09 11:46:48 +0300

    Added TruckProperties

---
+++ Ex03.GarageLogic/TruckProperties.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex03.GarageLogic
+{
+	class TruckProperties
+	{
+		private readonly float r_CargoholdVolume;
+		private readonly bool r_IsCargoholdCooled;
+
+		public TruckProperties(float i_CargoholdVolume, bool i_IsCargoholdCooled)
+		{
+			r_CargoholdVolume = i_CargoholdVolume;
+			r_IsCargoholdCooled = i_IsCargoholdCooled;
+		}
+
+		public float CargoholdVolume
+		{
+			get
+			{
+				return r_CargoholdVolume;
+			}
+		}
+
+		public bool IsCargoholdCooled
+		{
+			get
+			{
+				return r_IsCargoholdCooled;
+			}
+		}
+	}
+}
--- Ex03.GarageLogic/Vehical Types/Truck.cs
+++ Ex03.GarageLogic/Vehical Types/Truck.cs
@@ -6,29 +6,11 @@ namespace Ex03.GarageLogic.Vehical_Types
 {
 	class Truck : GasVehical
 	{
-		private readonly float r_CargoholdVolume;
-		private readonly bool r_IsCargoholdCooled;
+		private readonly TruckProperties r_TruckProperties = null;
 
 		public Truck(string i_Model, string i_LicensePlate, List<Wheel> i_Wheels, Enums.eFuelType i_FuelType, float i_MaxGasTankCapacity, float i_CurrentGasVolume, bool i_IsCargoholdCooled, float i_CargoholdVolume) : base(i_Model, i_LicensePlate, i_Wheels, i_FuelType, i_MaxGasTankCapacity, i_CurrentGasVolume)
 		{
-			r_CargoholdVolume = i_CargoholdVolume;
-			r_IsCargoholdCooled = i_IsCargoholdCooled;
-		}
-
-		public float CargoholdVolume
-		{
-			get
-			{
-				return r_CargoholdVolume;
-			}
-		}
-
-		public bool IsCargoholdCooled
-		{
-			get
-			{
-				return r_IsCargoholdCooled;
-			}
+			r_TruckProperties = new TruckProperties(i_CargoholdVolume, i_IsCargoholdCooled);
 		}
 	}
 }
