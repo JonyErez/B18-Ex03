b81388419f9b72cde7d39e2184af1c0773b224b8
Niv Dunay <nivdu@mta.ac.il>
2018-05-09 12:41:57 +0300

    remove in ctors

--- Ex03.GarageLogic/Vehical Types/GasCar.cs
+++ Ex03.GarageLogic/Vehical Types/GasCar.cs
@@ -8,9 +8,8 @@ namespace Ex03.GarageLogic.Vehical_Types
 	{
 		private readonly CarProperties r_carProperties = null;
 
-		public GasCar(string i_Model, string i_LicensePlate, List<Wheel> i_Wheels, Enums.eFuelType i_FuelType, float i_MaxGasTankCapacity, float i_CurrentGasVolume, Enums.eColor i_Color, Enums.eNumberOfDoors i_NumberOfDoors) : base(i_Model, i_LicensePlate, i_Wheels, i_FuelType, i_MaxGasTankCapacity, i_CurrentGasVolume)
+		public GasCar(string i_Model, string i_LicensePlate, List<Wheel> i_Wheels, Enums.eFuelType i_FuelType, float i_MaxGasTankCapacity, float i_CurrentGasVolume) : base(i_Model, i_LicensePlate, i_Wheels, i_FuelType, i_MaxGasTankCapacity, i_CurrentGasVolume)
 		{
-			r_carProperties = new CarProperties(i_Color, i_NumberOfDoors);
 		}
 	}
 }
